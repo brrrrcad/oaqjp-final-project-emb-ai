4f6b71e4acdea390987a4c01fbca071741e89c54
brrrrcad <burghardist@gmail.com>
2026-09-20 14:42:36 -0400

    Task 4

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+HEADERS = {
+    "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+def emotion_detector(text_to_analyze):
+    data = json.dumps({ "raw_document": { "text": text_to_analyze } })
+    response = requests.post(url=URL, headers=HEADERS, data=data)
+    response_json = json.loads(response.text)
+    emotions_dict = response_json["emotionPredictions"][0]["emotion"]
+    max_value = max(emotions_dict.values())
+    for emotion in emotions_dict.keys():
+        if emotions_dict[emotion] == max_value:
+            dominant_emotion = emotion
+    emotions_dict["dominant_emotion"] = dominant_emotion
+    return emotions_dict
+
